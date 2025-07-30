668092dccd5a474c1ce8ec66dbb482f033cc3ae5
Prakash Raut <prakash_raut@outlook.com>
2025-07-30 16:30:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const timerEle = document.getElementById("timer");
+
+let currentDate = new Date();
+
+setInterval(() => {
+	currentDate = new Date();
+	timerEle.innerText = currentDate.toLocaleString();
+}, 1000)
